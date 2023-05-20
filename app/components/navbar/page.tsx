a00593aeb238d974ad56013227d628e9256498d8
Minh Ha <21020621@vnu.edu.vn>
2023-05-20 20:56:10 +0700

    update font

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
           <label tabIndex={0} className="btn btn-ghost text-secondary lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-secondary rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-info rounded-box w-52">
             <li><a>Tin tức</a></li>
             <li><a>Hướng dẫn</a></li>
             <li><a>Giới thiệu</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
       <NavBar />
       <div className='flex flex-col bg-base-100 w-full h-3/4 whitespace-nowrap grow sm:grid sm:grid-cols-2 sm:p-10'>
         <div className='text-center font-bold pt-28 uppercase sm:text-left sm:px-10 sm:pt-0 sm:my-auto'>
-          <div className="text-4xl text-accent my-3 sm:text-5xl sm:my-5 lg:text-7xl">Hệ thống</div>
-          <div className="text-2xl text-primary sm:text-3xl lg:text-5xl">quản lý đăng kiểm</div>
+          <div className="text-5xl text-accent my-3 sm:text-6xl sm:my-5 lg:text-8xl font-800">Hệ thống</div>
+          <div className="text-2xl text-primary sm:text-3xl lg:text-5xl font-700">quản lý đăng kiểm</div>
         </div>
         <div className='flex justify-center gap-5 sm:hidden py-5'>
           <a className="btn btn-outline btn-primary btn-sm">Tạo tài khoản</a>
           <a className="btn btn-primary btn-sm">Đăng nhập</a>
-        </div>        
+        </div>
         <div className='relative h-1/2 sm:h-full'>
           <Image
             src="/car.png"
@@ -30,7 +30,7 @@ export default function Home() {
             sizes="100vw"
             className="w-full top-0 left-0 object-contain rounded-2xl"
           />
-        </div>      
+        </div>
       </div>
       <footer className="footer shrink p-5 bg-neutral text-neutral-content sm:px-10 sm:pb-10">
         <div>
