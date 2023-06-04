ba0c311526261c6b4b33c456d72f08f1cbbcd27f
Minh Ha <21020621@vnu.edu.vn>
2023-06-04 10:14:44 +0700

    update

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
       <div className="navbar-start">
         <div className="dropdown dropdown-hover">
           <label tabIndex={0} className="btn btn-ghost text-base-100-info lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="current" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="dropdown-content menu mt-3 p-2 shadow bg-base-100 rounded-box w-52">
             <li><a>Tin tức</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import NavBar from './components/navbar/page'
+import Modal from './components/modal/page';
 import type { Metadata } from 'next'
 
 
 export const metadata: Metadata = {
   title: 'Registry Total',
   description: 'Welcome to Registry Total',
+  icons: {icon: '/favicon.ico'}
 };
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-between p-[2rem]">
+    <div className="h-screen flex flex-col justify-between p-[2rem] bg-white">
       <NavBar />
       <div className='flex justify-center gap-5 sm:hidden py-5'>
         <a className="btn btn-outline btn-primary btn-sm">Tạo tài khoản</a>
@@ -34,16 +36,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <footer className="footer shrink p-5 bg-neutral text-neutral-content sm:px-10 sm:pb-10">
-        <div>
-          <div className='h-1 w-1/3 my-2 bg-neutral-content'></div>
-          <p>
-            INT3306 20 - Phát triển ứng dụng Web<br />
-            Lê Hoàng Minh Hà - 21020621<br />
-            Hoàng Thị Thu Hà - 21020189
-          </p>
-        </div>
-      </footer>
+      {/* <Modal isOpen/> */}
     </div>
   )
 }
