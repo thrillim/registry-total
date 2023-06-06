8ad943c84902d05bdf3b8befa43e80774900e234
thrillim <hauniuni23@gmail.com>
2023-06-06 17:03:28 +0700

    custom next-auth sign in page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Providers from './Providers'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,11 +11,13 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
