5a0e4ff02ed07656d83fe8b42ddc91276165f48f
thrillim <86657389+thrillim@users.noreply.github.com>
2023-06-12 01:06:11 +0700

    overview rename

--- app/Register/dashboard/summary.tsx
+++ app/Register/dashboard/summary.tsx
@@ -12,7 +12,7 @@ export default function RegisterSummary() {
     if (isLoading) return 'Loading overview...';
     return (<>
         <div className="card w-full shadow-lg text-black p-4 mt-7 card-bordered">
-            <h2 className="text-3xl font-bold text-accent mx-auto card-title">Tổng quan về số lượng xe đăng kiểm</h2>
+            <h2 className="text-3xl font-bold text-accent mx-auto card-title">Tổng quan về số lượt đăng kiểm thành công</h2>
             <div className="container flex flex-row w-full gap-5 card-body">
                 <div className="w-1/3 card card-bordered flex flex-col py-5 border-4">
                     <div className="text-2xl font-bold text-center">Tháng trước</div>
