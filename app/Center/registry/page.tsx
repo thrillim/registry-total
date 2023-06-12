0287c67e7986e66f898a6d928afbe3f44bb42506
Minh Ha <21020621@vnu.edu.vn>
2023-06-12 21:25:11 +0700

    center registry: front end

--- app/Center/registry/page.tsx
+++ app/Center/registry/page.tsx
@@ -10,17 +10,17 @@ export const metadata: Metadata = {
 }
 
 const check = {
-    check1: true,
+    check1: false,
     check2: false,
     check3: false,
-    check4: true,
+    check4: false,
     check5: false,
 }
 
 export default async function CarRegistry() {
 
     return (
-        <div className="w-full min-h-screen bg-white text-black">
+        <div className="w-full min-h-screen bg-white text-black mx-auto">
             <div className='border-b z-50'>
                 <MainNav />
             </div>
@@ -30,79 +30,28 @@ export default async function CarRegistry() {
             </div>
 
             <div className="container w-full min-w-max min-h-screen 
-                my-20 mx-auto flex flex-col gap-10">
+                my-20 mx-auto flex flex-col gap-10 items-center">
 
                 <h2 className="font-bold text-3xl text-center text-primary">Kết quả kiểm tra</h2>
 
-                <div id="check1" tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100">
+                <details id="check1" tabIndex={0}
+                    className="collapse collapse-arrow  min-w-max max-w-lg border border-base-300 bg-base-100">
                     {(check.check1) ? (
-                        <h2 className='collapse-title text-xl font-semibold text-green-600'>
+                        <summary className='collapse-title text-xl font-semibold text-green-600'>
                             1. Kiểm tra nhận dạng tổng quát
-                        </h2>)
+                        </summary >)
                         : (
-                            <h2 className='collapse-title text-xl font-semibold text-red-600'>
+                            <summary className='collapse-title text-xl font-semibold text-red-600'>
                                 1. Kiểm tra nhận dạng tổng quát
-                            </h2>
+                            </summary>
                         )
                     }
-                    <TongQuat />
-                </div>
+                    <div className="collapse-content">
+                        <TongQuat />
+                    </div>
+                </details >
 
-                <div id="check2" tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100">
-                    {(check.check2) ? (
-                        <h2 className='collapse-title text-xl font-semibold text-green-600'>
-                            2. Kiểm tra phần trên của phương tiện
-                        </h2>)
-                        : (
-                            <h2 className='collapse-title text-xl font-semibold text-red-600'>
-                                2. Kiểm tra phần trên của phương tiện
-                            </h2>
-                        )
-                    }
-
-                </div>
-
-                <div id="check3" tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100">
-                    {(check.check3) ? (
-                        <h2 className='collapse-title text-xl font-semibold text-green-600'>
-                            3. Kiểm tra hiệu quả phanh và trượt ngang
-                        </h2>)
-                        : (
-                            <h2 className='collapse-title text-xl font-semibold text-red-600'>
-                                3. Kiểm tra hiệu quả phanh và trượt ngang
-                            </h2>
-                        )
-                    }
-
-                </div>
-
-                <div id="check4" tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100">
-                    {(check.check4) ? (
-                        <h2 className='collapse-title text-xl font-semibold text-green-600'>
-                            4. Kiểm tra môi trường
-                        </h2>)
-                        : (
-                            <h2 className='collapse-title text-xl font-semibold text-red-600'>
-                                4. Kiểm tra môi trường
-                            </h2>
-                        )
-                    }
-
-                </div>
-
-                <div id="check5" tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100">
-                    {(check.check5) ? (
-                        <h2 className='collapse-title text-xl font-semibold text-green-600'>
-                            5. Kiểm tra phần dưới của phương tiện
-                        </h2>)
-                        : (
-                            <h2 className='collapse-title text-xl font-semibold text-red-600'>
-                                5. Kiểm tra phần dưới của phương tiện
-                            </h2>
-                        )
-                    }
-
-                </div>
+                
                 {(check.check1 && check.check2 && check.check3 && check.check4 && check.check5)
                     ?
                     (<div>
