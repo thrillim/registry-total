a6afd41ad1544f707ad4aa59d93b7407570e1788
thrillim <86657389+thrillim@users.noreply.github.com>
2023-06-12 23:30:21 +0700

    fix search bar: not receive dumb data

--- app/Center/[centerId]/registry/searchBar.tsx
+++ app/Center/[centerId]/registry/searchBar.tsx
@@ -1,39 +1,61 @@
 'use client'
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { TextField } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
+import useSWR from 'swr';
+const fetcher = (url: RequestInfo | URL) =>
+  fetch(url).then((res) => res.json());
 
-const cars: string[] = [
-    "29A-12345",
-    "29A-12346",
-    "29A-12347",
-]
+// const cars: string[] = [
+//     "29A-12345",
+//     "29A-12346",
+//     "29A-12347",
+// ]
+
 
 export function SearchBar() {
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState<string | null>(null);
+
+    const { data: cars, error, isLoading } = useSWR(
+        'http://localhost:3000/api/getCarPlates',
+        fetcher
+      );
+    if (error) return 'Có lỗi xảy ra khi tải thông tin';
+    if (isLoading) return 'Đang tải thông tin....';
+
+    // // if cars is not undefind, do next step
+    // if (typeof cars !== undefined) {
+    // }
+
+    // filter all bienSo to an array
+    let carPlates = []
+    for (let i = 0; i < cars.length; i++) {
+        carPlates.push(cars[i].bienSo);
+    }
+    
 
     return (
         <>
             <div className="input">
                 <Stack spacing={2} direction="row" minWidth={150}>
                     <Autocomplete
-                        freeSolo
+                        value={value}
+                        onChange={(event: any, newValue: string | null) => {
+                            setValue(newValue);
+                        }}
+
+                        disablePortal
                         id="car"
-                        disableClearable
+                        clearOnEscape
                         fullWidth={true}
-                        options={cars.map((option) => option)}
+                        options={carPlates}
                         renderInput={(params) => (
                             <TextField
                                 {...params}
                                 label="Nhập biển số xe"
                                 fullWidth={true}
-                                InputProps={{
-                                    ...params.InputProps,
-                                    type: 'search',
-                                }}
-
                             />
                         )}
                         onChange={(event, value) => {
@@ -47,7 +69,8 @@ export function SearchBar() {
                         <div className="container mx-auto mt-5 font-medium text-lg text-center">
                             Biển số xe:
                             <span className="text-accent font-semibold"> {value}</span>
-                        </div>)}
+                        </div>)
+                }
             </div>
         </>
     )
