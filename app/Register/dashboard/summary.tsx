65733e9b4ad762549250dfe56bf26aac980b3745
Minh Ha <21020621@vnu.edu.vn>
2023-06-19 19:38:45 +0700

    update

--- app/Register/dashboard/summary.tsx
+++ app/Register/dashboard/summary.tsx
@@ -25,7 +25,7 @@ export default function RegisterSummary() {
                     <div className="text-center text-5xl font-extrabold text-green-700">{overview.previousQuarter}</div>
                 </div>
                 <div className="w-1/3 card card-bordered flex flex-col py-5 border-4  min-w-max">
-                    <div className="text-2xl font-bold text-center">Năm trước</div>
+                    <div className="text-2xl font-bold text-center">Từ đầu năm</div>
                     <div className="divider my-2"></div>
                     <div className="text-center text-5xl font-extrabold text-red-700">{overview.currentYear}</div>
                 </div>
